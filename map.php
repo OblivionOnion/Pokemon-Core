7f731855e5ccca381a236a4fd1244804f68edcc0
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:06:28 -0400

    Create map.php

---
+++ map.php
@@ -0,0 +1,825 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+ <?php
+
+  require_once 'gym_functions.php';
+
+
+
+
+
+  require_once 'config.php';
+
+require_once 'functions.php';
+
+  require_once 'sections/sidebarsin.php';
+
+mysql_connect("pokemoncore.db.11569752.hostedresource.com", "pokemoncore", "Temping12!");
+
+mysql_select_db("pokemoncore");
+
+
+
+
+
+  if(isset($_SESSION['username']))
+
+{
+
+
+
+}else{
+
+ echo '<META HTTP-EQUIV="Refresh" Content="0; URL=login.php">';    
+
+    exit;
+
+}
+
+  ?>
+
+<div id="container">
+
+	<div id="banner">
+
+    </div>
+
+    
+
+    
+
+
+
+    
+
+    <div id="content">
+
+
+
+  <div class="CSSTableGenerator" >
+
+ 
+
+ <?php
+
+
+
+$map2 = (int) $_GET['map'];
+
+
+
+$ap2= mysql_real_escape_string($map2);
+
+$map = strip_tags($ap2);
+
+
+
+
+
+
+
+$uid = (int) $_SESSION['userid'];
+
+
+
+switch ($map) {
+
+	case '1':
+
+		$startX = 3;
+
+		$startY = 3;
+
+		$mapTitle = 'Grass Zone';
+
+		$tileArray = '
+
+			var map = [
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+			];
+
+		';
+
+	break;
+
+	
+
+	case '2':
+
+		$startX = 3;
+
+		$startY = 3;
+
+		$mapTitle = 'Water Zone';
+
+		$tileArray = '
+
+			var map = [
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+			];
+
+		';
+
+	break;
+
+	
+
+	case '3':
+
+		$startX = 3;
+
+		$startY = 3;
+
+		$mapTitle = 'Rock Zone';
+
+		$tileArray = '
+
+			var map = [
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+
+				[0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+
+				[0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+
+				[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+			];
+
+		';
+
+	break;
+
+	
+
+	case '4':
+
+		$startX = 3;
+
+		$startY = 3;
+
+		$mapTitle = 'Ice Zone';
+
+		$tileArray = '
+
+			var map = [
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+
+				[0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+			];
+
+		';
+
+	break;
+
+	
+
+	default:
+
+		
+
+		
+
+		 echo '<META HTTP-EQUIV="Refresh" Content="0; URL=map.php?map=1">';    
+
+    exit;  
+
+	break;
+
+}
+
+
+
+$time = time();
+
+$tenMinsAgo = $time - (60*10);
+
+$usersQuery = mysql_query("SELECT * FROM `users` WHERE `map_num`='{$map}' AND `map_lastseen`>='{$tenMinsAgo}'");
+
+$numUsersOnMap = mysql_num_rows($usersQuery);
+
+$usersArray = array();
+
+
+
+$i = 0; $onMap = false;
+
+while ($user = mysql_fetch_assoc($usersQuery)) {
+
+	if ($user['id'] == $uid) {
+
+		$startX = $user['map_x'];
+
+		$startY = $user['map_y'];
+
+		$onMap = true;
+
+		continue;
+
+	}
+
+	$usersArray[$i]['username'] = cleanHtml($user['username']);
+
+	$usersArray[$i]['id'] = (int) $user['id'];
+
+	$usersArray[$i]['x']  = (int) $user['map_x'];
+
+	$usersArray[$i]['y']  = (int) $user['map_y'];
+
+	$usersArray[$i]['sprite']  = (int) $user['map_sprite'];
+
+	$i++;
+
+}
+
+
+
+mysql_query("UPDATE `users` SET `map_num`='{$map}', `map_x`='{$startX}', `map_y`='{$startY}', `map_lastseen`='{$time}' WHERE `id`='{$uid}'");
+
+if (!$onMap) {
+
+	$numUsersOnMap++;
+
+}
+
+
+
+$mySpriteQuery = mysql_query("SELECT `map_sprite` FROM `users` WHERE `id`='{$uid}' LIMIT 1");
+
+$mySprite = mysql_fetch_assoc($mySpriteQuery);
+
+$mySprite = $mySprite['map_sprite'];
+
+
+
+
+
+$canCatchLegends          = canCatchLegends($uid);
+
+$_SESSION['catchLegneds'] = $canCatchLegends;
+
+
+
+$legendsMsg = $canCatchLegends ? 'You can catch legendary pokemon!' : 'You can <strong>not</strong> catch legendary pokemon!' ;
+
+
+
+
+
+?>
+
+
+
+
+
+<div class="sub-content"> 
+
+<div class="header">..::  Map - <?php echo $mapTitle; ?> ::..</div>
+
+	
+
+<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+
+<script>
+
+
+
+
+
+	var movements = {
+
+		upleft    : {up: -1, left: -1},
+
+		up        : {up: -1, left:  0},
+
+		upright   : {up: -1, left:  1},
+
+		left      : {up:  0, left: -1},
+
+		right     : {up:  0, left:  1},
+
+		downleft  : {up:  1, left: -1},
+
+		down      : {up:  1, left:  0},
+
+		downright : {up:  1, left:  1},
+
+	};
+
+	
+
+	var x = <?php echo $startX; ?>, y = <?php echo $startY; ?>;
+
+	
+
+	<?php echo $tileArray; ?>
+
+	
+
+	function moveSprite(key) {
+
+		var sprite = document.getElementById('mySprite');
+
+		var newX = x + movements[key]['left'];
+
+		var newY = y + movements[key]['up'];
+
+		
+
+		if (map[newY][newX] == 1) {
+
+			x = newX;
+
+			y = newY;
+
+			sprite.style.left = x*16 + 'px';
+
+			sprite.style.top = (y*16)-4 + 'px';
+
+			
+
+			$("#result").html('<img src="images/loading.gif" />');
+
+			$.get('map_ajax.php?map=<?php echo $map; ?>&x='+x+'&y='+y, function(result) {
+
+				var res = jQuery.parseJSON(result);
+
+				if ( typeof res.name !== "undefined" ) {
+
+					var caught = res.caught == 1 ? ' <img src="images/pb.gif" alt="X" />' : '' ;
+
+					
+
+					var html = '';
+
+					html += '<p>';
+
+					html += '    <img src="images/pokemon/'+res.name+'.png" /><br />';
+
+					html += '    A wild '+res.name+' appered!'+caught+'<br />';
+
+					html += '    Level: '+res.level+'';
+
+					html += '</p>';
+
+					html += '<form action="battle.php" method="post">';
+
+					html += '    <input type="submit" value="Battle!" />';
+
+					html += '</form>';
+
+					
+
+					$("#result").html(html);
+
+				} else if ( typeof res.money !== "undefined" ) {
+
+					$("#result").html('<p>You found $'+res.money+' on the floor!</p>');
+
+				} else {
+
+					$("#result").text('You did not find any Pokemon.');
+
+				}
+
+			});
+
+		}
+
+	}
+
+	
+
+	var users = <?php echo json_encode($usersArray); ?>;
+
+	function addUsersToMap() {
+
+		for (var i=0; i<users.length; i++) {
+
+			var user = users[i];
+
+			
+
+			var image = document.createElement('img');
+
+			image.src = 'images/sprites/'+ user.sprite +'.png';
+
+			image.title = user.username;
+
+			image.style.position = 'absolute';
+
+			image.style.top = ((user.y*16)-4) + 'px';
+
+			image.style.left = (user.x*16) + 'px';
+
+			
+
+			
+
+
+
+			$(image).insertAfter('#mySprite');
+
+
+
+			
+
+		}
+
+	}
+
+	
+
+	window.addEventListener('load', addUsersToMap, false);
+
+</script>
+
+
+
+
+
+
+
+<table style="margin: 20px auto; padding: 10px;">
+
+	<tr>
+
+		<td>
+
+			<div style="background-image: url('images/maps/map<?php echo $map; ?>.png'); width: 400px; height: 400px; position: relative;" id="map">
+
+				<img src="images/sprites/<?php echo $mySprite; ?>.png" id="mySprite" title="<?php echo cleanHtml($_SESSION['username']); ?>" style="position: absolute; top: <?php echo (($startY*16)-4); ?>px; left: <?php echo ($startX*16); ?>px; z-index: 999;" />
+
+			</div>
+
+			<div style="clear: both;"></div>
+
+		</td>
+
+		<td style="width: 250px; text-align: center; vertical-align: top;">
+
+			<table style="margin: 0px auto 50px auto;">
+
+				<tr>
+
+					<td><img src="images/arrows/upleft.png" onclick="moveSprite('upleft');" /></td>
+
+					<td><img src="images/arrows/up.png" onclick="moveSprite('up');" /></td>
+
+					<td><img src="images/arrows/upright.png" onclick="moveSprite('upright');" /></td>
+
+				</tr>
+
+				<tr>
+
+					<td><img src="images/arrows/left.png" onclick="moveSprite('left');" /></td>
+
+					<td style="text-align: center; vertical-align: middle;"><img src="images/sprites/3.png" /></td>
+
+					<td><img src="images/arrows/right.png" onclick="moveSprite('right');" /></td>
+
+				</tr>
+
+				<tr>
+
+					<td><img src="images/arrows/downleft.png" onclick="moveSprite('downleft');" /></td>
+
+					<td><img src="images/arrows/down.png" onclick="moveSprite('down');" /></td>
+
+					<td><img src="images/arrows/downright.png" onclick="moveSprite('downright');" /></td>
+
+				</tr>
+
+			</table>
+
+			
+
+			<div id="result"></div>
+
+		</td>
+
+	</tr>
+
+	<tr>
+
+		<td>
+
+			<p style="text-align: center; font-size: small;">
+
+				Users on this map: <?php echo $numUsersOnMap; ?><br />
+
+				<span style="font-size: smaller;"><?php echo $legendsMsg; ?></span>
+
+			</p>
+
+		</td>
+
+	</tr>
+
+</table>
+
+</div>
+
+
+
+  
+
+
+
+
+
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+
+
+
+
+
+   
+
+        
+
+<center><script type="text/javascript"><!--
+
+google_ad_client = "ca-pub-9678842478299123";
+
+/* Pokemon Core Ads2 */
+
+google_ad_slot = "8835563699";
+
+google_ad_width = 468;
+
+google_ad_height = 60;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script></center>
+
+	
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <div id="copyright">
+
+		Footer
+
+		</div>
+
+    </div>
+
+
+
+    
+
+    
+
+    <div id="footer">
+
+				<div align="center">Pokemon Core Rpg  - Present<br />
+
+				  This site is not affilliated with Nintendo. The Pokemon Company, Creatures, or GameFreak
+
+	  </div>  </div><center><script type="text/javascript"><!--
+
+google_ad_client = "ca-pub-9678842478299123";
+
+/* Pokemon Core Ads */
+
+google_ad_slot = "6161298895";
+
+google_ad_width = 728;
+
+google_ad_height = 90;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script></center>
+
+
+
+</div>
+
+</body>
+
+</html>
