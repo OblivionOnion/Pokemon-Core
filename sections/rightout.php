bb804bb06f204e1e19c5cef5b9c604c12b2a20ae
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:33:01 -0400

    Create rightout.php

---
+++ sections/rightout.php
@@ -0,0 +1,11 @@
+
+<div id="rightnav">
+	<ul>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+	</ul>
+</div>
