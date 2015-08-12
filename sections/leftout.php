a38a844c9571a2519bc4864053e1d3f4ccb649ae
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:29:38 -0400

    Create leftout.php

---
+++ sections/leftout.php
@@ -0,0 +1,13 @@
+<div id="leftnav">
+	<ul>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">About</a></li>
+		<li><a href="">Contact</a></li>
+		<li><a href="">Register<div class="new">NEW</div></a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+	</ul>
+</div>
