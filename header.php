e29df18c1c72e53e363d3e71dd4d70c6d5e8ed37
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:11:45 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		<title>Pokemon Core - Online Pokemon RPG</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+<body>
+<div id="fix">DO NOT DELETE</div>
+
+<div id="header">
+</div>
+
+<div id="bg2"></div>
+<div id="bg3"></div>
+
+<div id="leftnav">
+	<ul>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">About</a></li>
+		<li><a href="">Contact</a></li>
+		<li><a href="">Register<div class="new">NEW</div></a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+	</ul>
+</div>
+
+<div id="rightnav">
+	<ul>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li><a href="">testing</a></li>
+		<li class="navtitle">Navigation</li>
+		<li><a href="">testing</a></li>
+	</ul>
+</div>
+
+<div id="content">
