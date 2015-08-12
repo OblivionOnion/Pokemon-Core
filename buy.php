cc08683564d58283f14f8ef81bf3feaa04432d4e
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:23:18 -0400

    Create buy.php

---
+++ buy.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+ <?php
+
+
+
+ require_once 'config.php';
+
+require_once 'functions.php';
+
+  require_once 'sections/title.php';
+
+  
+
+  if(isset($_SESSION['username']))
+
+{
+
+
+
+}else{
+
+ echo '<META HTTP-EQUIV="Refresh" Content="0; URL=login.php">';    
+
+    exit;
+
+}
+
+
+
+  ?>
+
+  
+
+
+
+<div id="container">
+
+	<div id="banner">
+
+    </div>
+
+    <div id="header">
+
+        <p>Pokemon Core Rpg V2.0</p>
+
+    </div>
+
+    
+
+  <?php
+
+  require_once 'sections/leftin.php';
+
+  
+
+  ?>
+
+    
+
+  <div id="content">
+
+
+
+    <div class="CSSTableGenerator" >
+
+    <table border="1" cellspacing="5" id="buy">
+
+		    <tbody>
+
+		      <tr>
+
+              
+
+              
+
+		        <td id="Name41217440"><div id="NP41217440" class="np">Vullaby ♀ - $840</div>
+
+		          <img src="TPPC%20Online%20RPG%20v8.0_files/629F.gif" alt="Vullaby" title="Vullaby" />
+
+		          <p>Released By #3227561<br />
+
+		            Released Less Than An Hour Ago<br />
+
+		            Level <span id="L41217440">5</span> <br />
+
+	              </p>
+
+		          <p class="center">Sorry, I'm too expensive!</p></td>
+
+                  
+
+                  
+
+		        <td id="Name41217437"><div id="NP41217437" class="np">Eevee ♂ - $840</div>
+
+		          <img src="TPPC%20Online%20RPG%20v8.0_files/133M.gif" alt="Eevee" title="Eevee" />
+
+		          <p>Released By #3227561<br />
+
+		            Released Less Than An Hour Ago<br />
+
+		            Level <span id="L41217437">5</span> <br />
+
+	              </p>
+
+		          <p class="center">Sorry, I'm too expensive!</p></td>
+
+                  
+
+                  
+
+                  
+
+	          </tr>
+
+	        </tbody>
+
+        </table>
+
+		  <p>&nbsp;</p>
+
+		  <p>&nbsp;</p>
+
+		  <p>&nbsp;</p>
+
+    </div> 
+
+    
+
+    <p align="center">&nbsp;</p>
+
+      <p>&nbsp;</p>
+
+      <center><script type="text/javascript"><!--
+
+google_ad_client = "ca-pub-9678842478299123";
+
+/* Pokemon Core Ads2 */
+
+google_ad_slot = "8835563699";
+
+google_ad_width = 728;
+
+google_ad_height = 90;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script></center>
+
+      <div id="copyright">
+
+		Footer
+
+	  </div>
+
+ </div>
+
+    <?php
+
+  require_once 'sections/righin.php';
+
+  
+
+  ?>
+
+    
+
+    
+
+    <div id="footer">
+
+				<div align="center">Pokemon Core Rpg  - Present<br />
+
+				  This site is not affilliated with Nintendo. The Pokemon Company, Creatures, or GameFreak
+
+	  </div>  </div><center><script type="text/javascript"><!--
+
+google_ad_client = "ca-pub-9678842478299123";
+
+/* Pokemon Core Ads */
+
+google_ad_slot = "6161298895";
+
+google_ad_width = 728;
+
+google_ad_height = 90;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script></center>
+
+
+
+</div>
+
+</body>
+
+</html>
