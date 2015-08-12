cf1ff318a409f3963b162f3171a3f9bf7d5ee76a
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:00:41 -0400

    Create read_pm.php

---
+++ read_pm.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+ <?php
+  require_once 'gym_functions.php';
+
+
+  require_once 'config.php';
+require_once 'functions.php';
+  require_once 'sections/sidebarsin.php';
+mysql_connect("pokemoncore.db.11569752.hostedresource.com", "pokemoncore", "Temping12!");
+mysql_select_db("pokemoncore");
+
+
+  if(isset($_SESSION['username']))
+{
+
+}else{
+ echo '<META HTTP-EQUIV="Refresh" Content="0; URL=login.php">';    
+    exit;
+}
+  ?>
+<div id="container">
+	<div id="banner">
+    </div>
+    
+    
+
+    
+    <div id="content">
+
+  <div class="CSSTableGenerator" >
+ 
+ <?php
+//We check if the user is logged
+if(isset($_SESSION['username']))
+{
+//We check if the ID of the discussion is defined
+if(isset($_GET['id']))
+{
+$id = intval($_GET['id']);
+//We get the title and the narators of the discussion
+$req1 = mysql_query('select title, user1, user2 from pm where id="'.$id.'" and id2="1"');
+$dn1 = mysql_fetch_array($req1);
+//We check if the discussion exists
+if(mysql_num_rows($req1)==1)
+{
+//We check if the user have the right to read this discussion
+if($dn1['user1']==$_SESSION['userid'] or $dn1['user2']==$_SESSION['userid'])
+{
+//The discussion will be placed in read messages
+if($dn1['user1']==$_SESSION['userid'])
+{
+	mysql_query('update pm set user1read="yes" where id="'.$id.'" and id2="1"');
+	$user_partic = 2;
+}
+else
+{
+	mysql_query('update pm set user2read="yes" where id="'.$id.'" and id2="1"');
+	$user_partic = 1;
+}
+//We get the list of the messages
+$req2 = mysql_query('select pm.timestamp, pm.message, users.id as userid, users.username, users.avatar from pm, users where pm.id="'.$id.'" and users.id=pm.user1 order by pm.id2');
+//We check if the form has been sent
+if(isset($_POST['message']) and $_POST['message']!='')
+{
+	$message = $_POST['message'];
+	//We remove slashes depending on the configuration
+	if(get_magic_quotes_gpc())
+	{
+		$message = stripslashes($message);
+	}
+	//We protect the variables
+	$message = mysql_real_escape_string(nl2br(htmlentities($message, ENT_QUOTES, 'UTF-8')));
+	//We send the message and we change the status of the discussion to unread for the recipient
+	if(mysql_query('insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read)values("'.$id.'", "'.(intval(mysql_num_rows($req2))+1).'", "", "'.$_SESSION['userid'].'", "", "'.$message.'", "'.time().'", "", "")') and mysql_query('update pm set user'.$user_partic.'read="yes" where id="'.$id.'" and id2="1"'))
+	{
+?>
+<div class="message">Your message has successfully been sent.<br />
+<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
+<?php
+	}
+	else
+	{
+?>
+<div class="message">An error occurred while sending the message.<br />
+<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
+<?php
+	}
+}
+else
+{
+//We display the messages
+?>
+<div class="content">
+<h1><?php echo $dn1['title']; ?></h1>
+<table class="messages_table">
+	<tr>
+    	<td class="author">User</td>
+        <td>Message</td>
+    </tr>
+<?php
+while($dn2 = mysql_fetch_array($req2))
+{
+?>
+	<tr>
+    	<td class="author center"><?php
+if($dn2['avatar']!='')
+{
+	echo '<img src="'.htmlentities($dn2['avatar']).'" alt="Image Perso" style="max-width:100px;max-height:100px;" />';
+}
+?><br /><a href="profile.php?id=<?php echo $dn2['userid']; ?>"><?php echo $dn2['username']; ?></a></td>
+    	<td class="left"><div class="date">Sent: <?php echo date('m/d/Y H:i:s' ,$dn2['timestamp']); ?></div>
+    	<?php echo $dn2['message']; ?></td>
+    </tr>
+<?php
+}
+//We display the reply form
+?>
+
+
+<?php
+
+
+if (isset($_POST["Deletemessage"])) {
+
+
+$id= mysql_real_escape_string($_GET['id']);
+$id2 = strip_tags($id);
+
+
+mysql_query("DELETE FROM pm WHERE id='".$id2."'");
+echo "This message has been removed.";
+
+
+}
+
+?>
+</table>
+<p>&nbsp;</p>
+<table width="200" border="0" align="center">
+  <tr>
+    <td scope="col">Reply</td>
+  </tr>
+  <tr>
+    <td>
+    <center>
+    <form action="read_pm.php?id=<?php echo $id; ?>" method="post">
+      <label for="message" class="center">Message</label>
+      <br />
+      <textarea cols="40" rows="5" name="message" id="message"></textarea>
+      <br />
+      <input type="submit" value="Send" />
+    </form></td>
+  </tr>
+</table>
+
+
+<p>&nbsp;</p>
+<table width="200" border="0" align="center">
+  <tr>
+    <td scope="col">Other options </td>
+  </tr>
+  <tr>
+    <td>
+    <center>
+    <form name="input" action="" method="Post">
+<input type="submit" value="Delete message" id="Deletemessage" name="Deletemessage">
+</form> </td>
+  </tr>
+</table>
+></div>
+
+  
+</center>
+<p><br />
+  
+  
+  
+</p>
+<h2>&nbsp;</h2>
+<div class="center"></div>
+</div>
+<?php
+}
+}
+else
+{
+	echo '<div class="message">You dont have the rights to access this page.</div>';
+}
+}
+else
+{
+	echo '<div class="message">This discussion does not exists.</div>';
+}
+}
+else
+{
+	echo '<div class="message">The discussion ID is not defined.</div>';
+}
+}
+else
+{
+	echo '<div class="message">You must be logged to access this page.</div>';
+}
+?>
+		<div class="foot"><a href="list_pm.php">Go to my personnal messages</a> </div>
+	
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+<center><script type="text/javascript"><!--
+google_ad_client = "ca-pub-9678842478299123";
+/* Pokemon Core Ads2 */
+google_ad_slot = "8835563699";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></center>
+   
+        
+
+	
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <div id="copyright">
+		Footer
+		</div>
+    </div>
+
+    
+    
+    <div id="footer">
+				<div align="center">Pokemon Core Rpg  - Present<br />
+				  This site is not affilliated with Nintendo. The Pokemon Company, Creatures, or GameFreak
+	  </div>  </div><center><!-- Begin BidVertiser code -->
+<SCRIPT LANGUAGE="JavaScript1.1" SRC="http://bdv.bidvertiser.com/BidVertiser.dbm?pid=548923&bid=1375006" type="text/javascript"></SCRIPT>
+<noscript><a href="http://www.bidvertiser.com">internet marketing</a></noscript>
+<!-- End BidVertiser code -->  </center>
+
+</div>
+</body>
+</html>
