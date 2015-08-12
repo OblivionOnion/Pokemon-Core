de17b0b340d48a571202c078bd1553f6271ba0b4
OblivionOnion <oblivionthomas@aol.com>
2015-08-12 09:04:43 -0400

    Create new_pm.php

---
+++ new_pm.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+ <?php
+  require_once 'gym_functions.php';
+
+
+  require_once 'config.php';
+require_once 'functions.php';
+  require_once 'sections/sidebarsin.php';
+mysql_connect("pokemoncore.db.11569752.hostedresource.com", "pokemoncore", "Temping12!");
+mysql_select_db("pokemoncore");
+
+
+  if(isset($_SESSION['username']))
+{
+
+}else{
+ echo '<META HTTP-EQUIV="Refresh" Content="0; URL=login.php">';    
+    exit;
+}
+  ?>
+<div id="container">
+	<div id="banner">
+    </div>
+    
+    
+
+    
+    <div id="content">
+
+  <div class="CSSTableGenerator" >
+ 
+ 
+ 
+<?php
+//We check if the user is logged
+if(isset($_SESSION['username']))
+{
+$form = true;
+$otitle = '';
+$orecip = '';
+$omessage = '';
+//We check if the form has been sent
+if(isset($_POST['title'], $_POST['recip'], $_POST['message']))
+{
+	$otitle = $_POST['title'];
+	$orecip = $_POST['recip'];
+	$omessage = $_POST['message'];
+	//We remove slashes depending on the configuration
+	if(get_magic_quotes_gpc())
+	{
+		$otitle = stripslashes($otitle);
+		$orecip = stripslashes($orecip);
+		$omessage = stripslashes($omessage);
+	}
+	//We check if all the fields are filled
+	if($_POST['title']!='' and $_POST['recip']!='' and $_POST['message']!='')
+	{
+		//We protect the variables
+		$title = mysql_real_escape_string($otitle);
+		$recip = mysql_real_escape_string($orecip);
+		$message = mysql_real_escape_string(nl2br(htmlentities($omessage, ENT_QUOTES, 'UTF-8')));
+		//We check if the recipient exists
+		$dn1 = mysql_fetch_array(mysql_query('select count(id) as recip, id as recipid, (select count(*) from pm) as npm from users where username="'.$recip.'"'));
+		if($dn1['recip']==1)
+		{
+			//We check if the recipient is not the actual user
+			if($dn1['recipid']!=$_SESSION['userid'])
+			{
+				$id = $dn1['npm']+1;
+				//We send the message
+				
+				
+
+if (preg_match("/http|www\.|\.(com|net|co\.uk)/", $message, $match)) :
+  print "You can not send urls !!";
+  die;
+endif;
+
+				
+				
+				
+				
+				
+				
+				
+				
+				if(mysql_query('insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read)values("'.$id.'", "1", "'.$title.'", "'.$_SESSION['userid'].'", "'.$dn1['recipid'].'", "'.$message.'", "'.time().'", "yes", "no")'))
+				{
+?>
+<div class="message">The message has successfully been sent.<br />
+<a href="list_pm.php">List of my personnal messages</a></div>
+<?php
+					$form = false;
+				}
+				else
+				{
+					//Otherwise, we say that an error occured
+					$error = 'An error occurred while sending the message';
+				}
+			}
+			else
+			{
+				//Otherwise, we say the user cannot send a message to himself
+				$error = 'You cannot send a message to yourself.';
+			}
+		}
+		else
+		{
+			//Otherwise, we say the recipient does not exists
+			$error = 'The recipient does not exists.';
+		}
+	}
+	else
+	{
+		//Otherwise, we say a field is empty
+		$error = 'A field is empty. Please fill of the fields.';
+	}
+}
+elseif(isset($_GET['recip']))
+{
+	//We get the username for the recipient if available
+	$orecip = $_GET['recip'];
+}
+if($form)
+{
+//We display a message if necessary
+if(isset($error))
+{
+	echo '<div class="message">'.$error.'</div>';
+}
+//We display the form
+?>
+
+
+  <table width="100%" border="1" class="gridtable" table="table">
+     <tr>
+       <td><h1>New Personnal Message</h1> </td>
+     </tr>
+     <tr>
+       <td>	
+       
+       
+	<center>
+    <form action="new_pm.php" method="post">
+		Please fill the following form to send a personnal message.<br />
+        <label for="title">Title</label><input type="text" value="<?php echo htmlentities($otitle, ENT_QUOTES, 'UTF-8'); ?>" id="title" name="title" /><br />
+        <label for="recip">Recipient<span class="small">(Username)</span></label><input type="text" value="<?php echo htmlentities($orecip, ENT_QUOTES, 'UTF-8'); ?>" id="recip" name="recip" /><br />
+        <label for="message">Message</label><textarea cols="40" rows="5" id="message" name="message"><?php echo htmlentities($omessage, ENT_QUOTES, 'UTF-8'); ?></textarea><br />
+        <input type="submit" value="Send" />
+    </form>
+    </center>
+     </td>
+     </tr>
+   </table>
+</div>
+</div>
+<?php
+}
+}
+else
+{
+	echo '<div class="message">You must be logged to access this page.</div>';
+}
+?>
+		<div class="foot"><a href="list_pm.php">Go to my personnal messages</a> </div>
+	</bo
+></div>
+
+  
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+  <center><script type="text/javascript"><!--
+google_ad_client = "ca-pub-9678842478299123";
+/* Pokemon Core Ads2 */
+google_ad_slot = "8835563699";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></center> 
+        
+
+	
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <div id="copyright">
+		Footer
+		</div>
+    </div>
+
+    
+    
+    <div id="footer">
+				<div align="center">Pokemon Core Rpg  - Present<br />
+				  This site is not affilliated with Nintendo. The Pokemon Company, Creatures, or GameFreak
+	  </div>  </div><center><script type="text/javascript"><!--
+google_ad_client = "ca-pub-9678842478299123";
+/* Pokemon Core Ads */
+google_ad_slot = "6161298895";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></center>
+</div>
+</body>
+</html>
